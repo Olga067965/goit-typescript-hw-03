6aeafb909a166d38e018df2e6cbd86c8bde38f41
Olga067965 <olgasavelieva5@gmail.com>
2023-11-08 01:08:22 +0100

    Update1

--- src/3.ts
+++ src/3.ts
@@ -8,6 +8,50 @@
 */
 
 // реалізація класу Wizard
+// class Wizard implements ICharacter, ISpellCaster {
+//   constructor(public name: string, public level: number) {
+//     if (this.level < 1) {
+//       throw new Error('Level too low');
+//     }
+//   }
+
+//   introduce(phrase: string): void {
+//     console.log(phrase + ', ' + this.name);
+//   }
+
+//   castSpell(): void {
+//     console.log('Casting a spell, behold my power!');
+//   }
+
+//   levelUp(): void {
+//     this.level++;
+//     console.log(`Level up! New level is ${this.level}`);
+//   }
+// }
+
+// // тестування класу
+// const wizard = new Wizard('Merlin', 15);
+
+// wizard.introduce('I am the mighty wizard');
+// wizard.castSpell();
+// wizard.levelUp();  // Level up! New level is 16
+
+// export { };
+
+// Інтерфейс ICharacter
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
+
+// Інтерфейс ISpellCaster
+interface ISpellCaster {
+  castSpell(): void;
+}
+
+// Реалізація класу Wizard
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
     if (this.level < 1) {
@@ -29,11 +73,11 @@ class Wizard implements ICharacter, ISpellCaster {
   }
 }
 
-// тестування класу
+// Тестування класу
 const wizard = new Wizard('Merlin', 15);
 
 wizard.introduce('I am the mighty wizard');
 wizard.castSpell();
 wizard.levelUp();  // Level up! New level is 16
 
-export {};
+export { };
--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,62 @@
+// const key = new Key();
+
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
+
+
+// export { };
+
+
+class Key {
+  generateSignature(): string {
+    return Math.random().toString(36).substring(7);
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor() {
+    this.key = new Key(); 
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+class House {
+  private doorLocked: boolean = true;
+  private key: Key | undefined;
+
+  unlockDoor(key: Key) {
+    const signature = key.generateSignature();
+
+    if (signature === this.key?.generateSignature()) {
+      this.doorLocked = false;
+      console.log('Двері відчинено.');
+    } else {
+      console.log('Невірний ключ. Двері залишаються зачиненими.');
+    }
+  }
+
+  enterHouse(person: Person) {
+    if (!this.doorLocked) {
+      console.log('Особа увійшла до будинку.');
+    } else {
+      console.log('Двері зачинені. Особа не може увійти.');
+    }
+  }
+}
+
 const key = new Key();
+const person = new Person();
+const house = new House();
 
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
+house.key = key; 
+house.unlockDoor(key);
+house.enterHouse(person);
 
 export {};
